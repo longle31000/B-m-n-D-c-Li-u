b8e8ead595b91bc9a0863755208cdfee4dafae34
phuongnvp <55146498+phuongnvp@users.noreply.github.com>
2023-05-09 23:12:07 +0700

    Update model

--- Boost_model.py
+++ Boost_model.py
@@ -1,10 +1,10 @@
-#%%
+#%% Import data
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import io
 import requests
-url = 'https://raw.githubusercontent.com/phuongnvp/BomonDuoclieu/main/data.csv'
+url = 'https://raw.githubusercontent.com/phuongnvp/BomonDuoclieu/Version-1.3/data.csv'
 s=requests.get(url).content
 df=pd.read_csv(io.StringIO(s.decode('utf-8')))
 df.columns = ['Pub_CID' + str(i+1) if i<100 else col for i, col in enumerate(df.columns)]
@@ -12,80 +12,85 @@ df.columns = ['Pub_Excipient' + str(i+1) if 99<i<200 else col for i, col in enum
 df.shape
 df["Outcome1"].value_counts()
 
-#%%
+#%% Define X and y
 y = df['Outcome1'].values
 X = df.drop(columns=["Outcome1", "API_CID", "Excipient_CID"], axis =1)
 print(X.shape)
 print(y.shape)
 
-#%%
+#%% Training set, validation set and test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.3, random_state=816)
-#%%
+X_train, X_remain, y_train, y_remain = train_test_split(X, y, test_size=.4, random_state=816)
+X_val, X_test, y_val, y_test = train_test_split(X_remain, y_remain, test_size=.5, random_state=817)
+
+#%% Handle imbalanced data
 from imblearn.over_sampling import SVMSMOTE
 svmsmote = SVMSMOTE(random_state=42)
 X_train_resampled, y_train_resampled = svmsmote.fit_resample(X_train, y_train)
 X_train_resampled
 
-#%%
+#%% Random forest and XGBoost
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.ensemble import RandomForestClassifier
 from xgboost import XGBClassifier
 model2 = RandomForestClassifier(n_estimators=100)
-model3 = XGBClassifier(max_depth=5, learning_rate=0.1, n_estimators=100)
+model3 = XGBClassifier(max_depth=5, learning_rate=0.5, n_estimators=100)
 
-#%%
-from sklearn.ensemble import VotingClassifier
-voting_model = VotingClassifier(estimators=[('rf', model2), ('xgb', model3)], voting='soft')
-boost_model = AdaBoostClassifier(base_estimator= voting_model, n_estimators=15, algorithm='SAMME')
+#%% Stack model
+from sklearn.ensemble import StackingClassifier
+from sklearn.linear_model import LogisticRegression
+lr = LogisticRegression(C=10)
+stack_model = StackingClassifier(estimators = [('rf', model2), ('xgb', model3)], final_estimator = lr)
 
-#%%
+#%% Stack model - Fit data
 class_weights = {0: 0.9, 1: 0.1}
-boost_model.fit(X_train_resampled, y_train_resampled, sample_weight=[class_weights[i] for i in y_train_resampled])
+stack_model.fit(X_train_resampled, y_train_resampled, sample_weight=[class_weights[i] for i in y_train_resampled])
 
-#%% Mô hình mặc định
+#%% Stack model - Evaluation
 from sklearn.metrics import accuracy_score, f1_score, precision_score
-y_pred = boost_model.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred)
-f1 = f1_score(y_test, y_pred)
-precision = precision_score(y_test, y_pred)
+y_pred = stack_model.predict(X_val)
+accuracy = accuracy_score(y_val, y_pred)
+f1 = f1_score(y_val, y_pred)
+precision = precision_score(y_val, y_pred)
 print("Accuracy:", accuracy)
 print("F1-score:", f1)
 print("Precision:", precision)
-#%%
+
+#%% Stack model - Confusion matrix
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
-cm = confusion_matrix(y_test, y_pred, labels=boost_model.classes_)
-disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=boost_model.classes_)
+cm = confusion_matrix(y_val, y_pred, labels=stack_model.classes_)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=stack_model.classes_)
 disp.plot()
 plt.show()
 
-#%% Chọn threshold dựa vào F1 score
+#%% Stack model - Select threshold based on F1 score
 from sklearn import metrics
-yhat = boost_model.predict_proba(X_test)
+yhat = stack_model.predict_proba(X_val)
 probs = yhat[:,1]
 thresholds = np.arange(0, 1, 0.001)
 def to_labels(pos_probs, threshold):
  return (pos_probs >= threshold).astype('int')
-scores = [f1_score(y_test, to_labels(probs, t)) for t in thresholds]
+scores = [f1_score(y_val, to_labels(probs, t)) for t in thresholds]
 ix = np.argmax(scores)
+threshold1 = thresholds[ix]
 print('Threshold=%.3f, F-Score=%.5f' % (thresholds[ix], scores[ix]))
 
-#%%
-y_pred_2 = (boost_model.predict_proba(X_test)[:,1] >= thresholds[ix]).astype(bool)
-accuracy_2 = accuracy_score(y_test, y_pred_2)
-f1_2 = f1_score(y_test, y_pred_2)
-precision_2 = precision_score(y_test, y_pred_2)
+#%% Stack model - Evaluate new threshold
+y_pred_2 = (stack_model.predict_proba(X_val)[:,1] >= thresholds[ix]).astype(bool)
+accuracy_2 = accuracy_score(y_val, y_pred_2)
+f1_2 = f1_score(y_val, y_pred_2)
+precision_2 = precision_score(y_val, y_pred_2)
 print("Accuracy:", accuracy_2)
 print("F1-score:", f1_2)
 print("Precision:", precision_2)
 
-#%%
-cm_2 = confusion_matrix(y_test, y_pred_2, labels=boost_model.classes_)
-disp = ConfusionMatrixDisplay(confusion_matrix=cm_2, display_labels=boost_model.classes_)
+#%% Stack model - Confusion matrix of new model
+cm_2 = confusion_matrix(y_val, y_pred_2, labels=stack_model.classes_)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm_2, display_labels=stack_model.classes_)
 disp.plot()
 plt.show()
 
-#%% Chọn threshold bằng thuật toán GHOST (doi.org/10.1021/acs.jcim.1c00160)
+#%% Stack model - Select threshold using GHOST algorithm (doi.org/10.1021/acs.jcim.1c00160)
 import ghostml
 from sklearn import metrics
 import numpy as np
@@ -100,27 +105,37 @@ def calc_metrics(labels_test, test_probs, threshold):
     print(metrics.classification_report(labels_test,scores))
     return 
 
-#%%
-train_probs = boost_model.predict_proba(X_train_resampled)[:,1]
+#%% Calculate threshold
+train_probs = stack_model.predict_proba(X_train_resampled)[:,1]
 thresholds = np.round(np.arange(0,1,0.001),2)
-threshold1 = ghostml.optimize_threshold_from_predictions(y_train_resampled, train_probs, thresholds, ThOpt_metrics = 'Kappa') 
-y_pred_3 = (boost_model.predict_proba(X_test)[:,1] >= threshold1).astype(bool)
-calc_metrics(y_test, y_pred_3, threshold = threshold1)
+threshold2 = ghostml.optimize_threshold_from_predictions(y_train_resampled, train_probs, thresholds, ThOpt_metrics = 'Kappa') 
+y_pred_3 = (stack_model.predict_proba(X_val)[:,1] >= threshold2).astype(bool)
+calc_metrics(y_val, y_pred_3, threshold = threshold2)
 
-#%%
-accuracy_3 = accuracy_score(y_test, y_pred_3)
-f1_3 = f1_score(y_test, y_pred_3)
-precision_3 = precision_score(y_test, y_pred_3)
+#%% Stack model - Evaluate new threshold
+accuracy_3 = accuracy_score(y_val, y_pred_3)
+f1_3 = f1_score(y_val, y_pred_3)
+precision_3 = precision_score(y_val, y_pred_3)
 print("Accuracy:", accuracy_3)
 print("F1-score:", f1_3)
 print("Precision:", precision_3)
 
-#%%
-cm_3 = confusion_matrix(y_test, y_pred_3, labels=boost_model.classes_)
-disp = ConfusionMatrixDisplay(confusion_matrix=cm_3, display_labels=boost_model.classes_)
+#%% Stack model - Confusion matrix of new model
+cm_3 = confusion_matrix(y_val, y_pred_3, labels=stack_model.classes_)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm_3, display_labels=stack_model.classes_)
 disp.plot()
 plt.show()
 
-#%%
-import joblib
-joblib.dump(boost_model, 'F:\Phuong\Project\API Excipient interaction\Final\model100.pkl')
+#%% Performance on test set
+y_pred_4 = (stack_model.predict_proba(X_test)[:,1] >= threshold1).astype(bool)
+calc_metrics(y_test, y_pred_4, threshold = threshold1)
+accuracy_4 = accuracy_score(y_test, y_pred_4)
+f1_4 = f1_score(y_test, y_pred_4)
+precision_4 = precision_score(y_test, y_pred_4)
+print("Accuracy:", accuracy_4)
+print("F1-score:", f1_4)
+print("Precision:", precision_4)
+cm_4 = confusion_matrix(y_test, y_pred_4, labels=stack_model.classes_)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm_4, display_labels=stack_model.classes_)
+disp.plot()
+plt.show()
