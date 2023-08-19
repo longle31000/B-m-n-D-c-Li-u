b2d2031aee2c6d134e9f10bafdf6bbb9cbdf6067
phuongnvp <55146498+phuongnvp@users.noreply.github.com>
2023-08-19 15:01:37 +0700

    Update model_deploy.py

--- model_deploy.py
+++ model_deploy.py
@@ -26,12 +26,12 @@ def get_cid(api, option):
 
 #%%
 st.title('Drug - Excipient Interaction v1.4')
-col1, col2 = st.columns(2)
+col1, col2 = st.columns([1,3])
 with col1: 
     option1 = st.selectbox('Column 1', ['Name', 'PubChem ID', 'SMILES'])
 with col2:
     API_CID = st.text_input('Enter the API')
-col3, col4 = st.columns(2)
+col3, col4 = st.columns([1,3])
 with col3: 
     option3 = st.selectbox('Column 3', ['Name', 'PubChem ID', 'SMILES'])
 with col4:
